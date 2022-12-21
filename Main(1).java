0fcfe8b5c497daf80135bdbb91acebe351e5a424
Darya <darya.ogurtsova.99@mail.ru>
2022-12-21 20:41:47 +0300

    Добавлено задание 1

---
+++ Main(1).java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(rnd.nextInt(1, 10));
+        }
+
+        LinkedList<Integer> reversList = new LinkedList<>();
+        for (Integer i : list) {
+            reversList.add(i);
+        }
+
+        int i = 0;
+        while (i != reversList.size() - 1) {
+            reversList.add(reversList.size()-i, reversList.getFirst());
+            reversList.removeFirst();
+            i++;
+        }
+        System.out.println("linkList случайных чисел от 1 до 10: ");
+        System.out.println(list);
+        System.out.println("Revers linkList: ");
+        System.out.println(reversList);
+
+    }
+
+}
